58e3bc6bf193752a4229159fb9a4c6274fd73ddf
Ноутбук <notebook@mail.ri>
2020-06-10 14:54:42 +0300

    добавлена библиотека для работы с excel

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,6 +13,9 @@ class ComposerAutoloaderInitb6c296f239e13c1fea869fa18da7c405
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
